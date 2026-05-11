7c7ef4e86ab88f7504ccc3e8826766ff3e28846e
Mohammad Tohidnia <tohidnia55@gmail.com>
2026-05-11 21:57:48 +0200

    Tested PWM and Delay successfully

--- Objects/my_timer.d
+++ Objects/my_timer.d
@@ -1,4 +1,4 @@
-./objects/main.o: Src\main.c E:\Keil\Arm\ARMCLANG\include\stdio.h \
+./objects/my_timer.o: Drivers\my_timer.c Drivers\my_timer.h \
   E:\Keil\Arm\packs\Keil\STM32F1xx_DFP\2.4.1\Device\Include\stm32f10x.h \
   RTE\_Target_1\RTE_Components.h \
   E:\Keil\Arm\packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm3.h \
@@ -6,5 +6,4 @@
   E:\Keil\Arm\packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
   E:\Keil\Arm\packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
   E:\Keil\Arm\packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
-  E:\Keil\Arm\packs\Keil\STM32F1xx_DFP\2.4.1\Device\Include\system_stm32f10x.h \
-  Drivers\my_gpio.h Drivers\my_rcc.h Drivers\my_timer.h
+  E:\Keil\Arm\packs\Keil\STM32F1xx_DFP\2.4.1\Device\Include\system_stm32f10x.h
--- Src/main.c
+++ Src/main.c
@@ -1,49 +1,47 @@
+#include <stdio.h>
 #include "stm32f10x.h"
 #include "my_gpio.h"
 #include "my_rcc.h"
+#include "my_timer.h"
 
-
-void delay(volatile uint32_t);
-
-void EXTI9_5_IRQHandler(void)
+/*void EXTI9_5_IRQHandler(void)
 {
 	GPIO_ClearInterruptFlag(5) ;
 	
 	GPIO_TogglePin(GPIOC, 13);
 
-}
+}*/
 
-
+volatile float k = 0;
 
 int main(void) {
 		
 	// Set the maximum clk for peripherals
-	//SystemClock_Config_72MHz();
+	SystemClock_Config_72MHz();
+	// Initialize Systick to processer clock
+	SysTick_Init();
 	
-	// Enable the clock for Port C, A
-	GPIO_EnableClock(GPIOC);
 	GPIO_EnableClock(GPIOA);
+	GPIO_InitPin(GPIOA, 0, GPIO_MODE_AF_PP);
+	GPIO_WritePin(GPIOA, 0, 1);
 	
-	// Configure PC13 as a Push-Pull Output
-	GPIO_InitPin(GPIOC, 13, GPIO_MODE_OUTPUT_PP);
-	GPIO_WritePin(GPIOC, 13, 1);
+	PWM_Init(TIM2, 1, 1000) ;
+	PWM_SetDutyCycle(TIM2, 1, 0);
+	PWM_Start(TIM2);
 	
-	GPIO_InitPin(GPIOA, 5, GPIO_MODE_INPUT_FLOATING) ;
-	GPIO_ConfigInterrupt(GPIOA, 5, EXTI_TRIGGER_RISING);
-	GPIO_EnableInterrupt(5, 0) ;
-	
-	
+	float fade = 1.0f ;
 	while(1) {
 			// Toggle the LED
-			//GPIO_TogglePin(GPIOC, 13);
-			//delay(3000000); 
+			k += fade ;
+			if (k > 100.0f || k < 0.0f){
+				fade = -fade;
+			}
+			PWM_SetDutyCycle(TIM2, 1, k);
+			
+		
+			
+			delay_ms(100);
 	}
 }
 
 
-// A simple, inaccurate delay function for testing
-void delay(volatile uint32_t count) {
-    while(count--) {
-        // Just burn CPU cycles
-    }
-}
